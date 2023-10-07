d12abbc4887f18a82fcf258552d46bb7cacc0ff6
Moath-Abdalrazeq <moathabdalhakeem1@gmail.com>
2023-10-08 01:44:13 +0300

    some edits in sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/SidebarMenu/Sidebar";
 import Home from "./components/HomePage/Home";
 import Profile from "./components/ProfilePage/Profile";
 import Messages from "./components/MassagesPage/Messages";
